db31bd99eaf7e8013a5cdea64cf59c0a1a1038d6
carolrevilla <carolrevilla0305@gmail.com>
2023-11-08 19:44:28 -0500

    nombre

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{background:"pink"}}>
+      <h1>CAROL REVILLA DAVILA</h1>
+      <h1>TURNO NOCHE</h1>
+      <h1>8/11/2023</h1>
+    </div>
+  )
+}
+
+export default App
